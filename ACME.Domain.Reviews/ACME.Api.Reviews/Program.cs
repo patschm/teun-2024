fe5e783bb8a827d1b1d1f7b24e74b8268dc3951a
unknown <pschmidt@pasit.nl>
2024-04-22 16:13:10 +0200

    domain1

---
+++ ACME.Domain.Reviews/ACME.Api.Reviews/Program.cs
@@ -0,0 +1,64 @@
+using ACME.Database.EntityFramework;
+using ACME.Database.EntityFramework.Repositories;
+using ACME.Domain.Reviews.Exceptions;
+using ACME.Domain.Reviews.Repositories;
+using ACME.Domain.Reviews.ValueObjects;
+using ACME.Infrastructure.Reviews.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// De Dependency Injector
+builder.Services.AddDbContext<ShopContext>(opts => {
+    var constr = builder.Configuration.GetConnectionString("Database");
+    opts.UseSqlServer(constr);
+});
+builder.Services.AddScoped<IReviewReadRepository, ReviewReadRepository>();
+builder.Services.AddScoped<IReviewWriteRepository, ReviewWriteRepository>();
+builder.Services.AddMediatR(conf => {
+    conf.RegisterServicesFromAssemblies(
+        ACME.Infrastructure.Reviews.AssemblyReference.Assembly
+        );
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/reviews", async ([FromServices] ISender sender, [FromQuery] int page = 1, [FromQuery] int amount = 10) => {
+    var result = await sender.Send(new ReadReviewsCommand(new ReviewParameters(page, amount)));
+    return Results.Ok(result);
+})
+.WithName("GetReviews")
+.WithOpenApi();
+
+app.MapGet("/reviews/{id:long}", async ([FromServices] ISender sender, [FromRoute]long id) =>
+{
+    try
+    {
+        //var handler = new ACME.Infrastructure.Reviews.Handlers.ReadReviewCommandHandler(app.Services.GetRequiredService<IReviewReadRepository>());
+        //var result = await  handler.Handle(new ReadReviewCommand(id));
+        var result = await sender.Send(new ReadReviewCommand(id));
+        return Results.Ok(result);
+    }
+    catch (ReviewNotFoundException)
+    {
+        return Results.NotFound(id);
+    }
+    
+})
+.WithName("GetReview")
+.WithOpenApi();
+
+app.Run();
