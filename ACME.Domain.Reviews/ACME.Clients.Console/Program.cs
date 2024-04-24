a2c3a87fa83eec52b6d7bc55818a23f299b513e4
unknown <pschmidt@pasit.nl>
2024-04-24 09:23:06 +0200

    daaylast

---
+++ ACME.Domain.Reviews/ACME.Clients.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ACME.Clients.Console;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("https://ps-acme-review.azurewebsites.net");
+
+        var response = await client.GetAsync("reviews");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            System.Console.WriteLine(data);
+        }
+
+        System.Console.ReadLine();
+    }
+}
